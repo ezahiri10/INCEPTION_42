37f4b63660adde066bfecf666d6d8c21938e2fb5
El Mustapha Zahiri <oz22wac@gmail.com>
2025-05-19 11:14:24 -0400

    123

--- requirements/mariadb/tools/init.sh
+++ requirements/mariadb/tools/init.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 
-
-s="yassine";
-
-
-
 service mariadb start
 
 
 sleep 5
 
 
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mariadb -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER_NAME}'@'%' IDENTIFIED BY '${MYSQL_USER_PASS}';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER_NAME}'@'%';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_NAME}'@'%';"
 mariadb -u root -e "FLUSH PRIVILEGES;"
 
 mysqladmin shutdown -u root
