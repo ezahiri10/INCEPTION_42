26a1402192207f1903edd7744872d3ea8d94db5f
ezahiri <zahiri@ezahiri.42.fr>
2025-05-21 17:47:39 +0100

    log

--- Makefile
+++ Makefile
@@ -2,12 +2,14 @@ all : build
 
 build :
 	@echo "Building the containers..."
-	docker compose -f srcs/ up -d
+	docker compose -f srcs/docker-compose.yml up -d
 	@echo "Containers are up and running"
 
 
 clean :
-	docker compose down
-	docker volume rm $(shell docker volume ls -q)
+	docker compose -f srcs/docker-compose.yml down
+	docker rm -f $(docker ps -a -q)  || true
+	docker volume prune -f
 	docker network prune -f
+
 
---
+++ srcs/file1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Stop all running containers
+echo "Stopping all running containers..."
+docker stop $(docker ps -q)
+
+# Remove all containers (both stopped and running)
+echo "Removing all containers..."
+docker rm $(docker ps -a -q)
+
+# Remove all networks (except default ones)
+echo "Removing all networks..."
+docker network prune -f
+
+sudo rm -rf /home/ezahiri/data/mariadb/*
+sudo rm -rf /home/ezahiri/data/wordpress/*
+sudo rm -rf /home/ezahiri/data/portainer/*
+# Remove all volumes
+echo "Removing all volumes..."
+docker volume prune -f
+
+# Remove all unused images (optional)
+echo "Removing all unused images..."
+docker image prune -a -f
+
+# Remove dangling images
+echo "Removing dangling images..."
+docker image prune -f
+
+# Final message
+echo "Cleanup complete!"
+
